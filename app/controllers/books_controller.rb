7a0deaf177a1b0ad7b88d6df241f14e5b03ed5be
Takuya Sako <135435911+hoisa3588@users.noreply.github.com>
2023-06-24 01:33:06 +0000

    [Add]-1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,15 +2,31 @@ class BooksController < ApplicationController
   def top
   end
   
-  def index
+  def new
+    @book = Book.new
   end  
  
+  def create
+    book = Book.new(book_params)
+  
+    book.save
+    
+    redirect_to '/top'
+  end
+  
+  def index
+    @books = Book.all
+  end
   def show
   end
 
   def edit
   end  
 
+ private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
 
-
+  end
 end
